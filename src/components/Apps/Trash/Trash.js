6cfbee3da78dc7591c459adc8e7aa8197d3523d6
ahamelin9 <hamel002@rangers.uwp.edu>
2026-05-06 20:07:41 -0500

    chore: clean up committed node_modules, dist, and redundant .js files to fix build

--- src/components/Apps/Trash/Trash.js
+++
@@ -1,7 +0,0 @@
-import { jsx as _jsx } from "react/jsx-runtime";
-//Styling
-import './Trash.css';
-const Trash = () => {
-    return (_jsx("div", { className: "trash-container", children: "This is the trash at the moment." }));
-};
-export default Trash;
